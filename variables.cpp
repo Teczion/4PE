989dd4d3b2445403ecd2a78421217acbbd6dca7b
Martin <martin_aj22@hotmail.com>
2020-07-21 16:51:35 -0600

    feat: variables

---
+++ variables.cpp
@@ -0,0 +1,13 @@
+// Ejemplo variables
+#include <iostream>
+using namespace std;
+
+int main(){
+	int var1 = 1;
+	char var2 = 'a';
+	float var3 = 13.5;
+	double var4 = 11.4;
+	bool var5 = true;
+	
+	return 0;
+}
