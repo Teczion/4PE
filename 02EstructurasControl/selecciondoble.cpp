32b4172dc2737344275289e5a1129b13e3b60140
Martin <martin_aj22@hotmail.com>
2020-07-28 17:06:03 -0600

    feat: ejemplos etructuras de control

---
+++ 02EstructurasControl/selecciondoble.cpp
@@ -0,0 +1,15 @@
+//Estructura compuesta o doble
+#include <iostream>
+using namespace std;
+
+main(){
+	int ed;
+	cout<<"Ingrese su edad: ";
+	cin>>ed;
+	if(ed>=18){
+		cout<<"Mayor de edad\n";
+	}else{
+		cout<<"No es mayor de edad\n";
+	}
+	cout<<"Fin del programa";
+}
