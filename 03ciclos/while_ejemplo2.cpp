214e7b9a13c5b9b50e112557c004ac25579d7ee3
Martin <martin_aj22@hotmail.com>
2020-08-05 23:58:42 -0600

    feat: ejemplo ciclos

---
+++ 03ciclos/while_ejemplo2.cpp
@@ -0,0 +1,19 @@
+//While ejemplo 2
+
+/*
+ *Imprime debo hacer la tarea 20 veces
+ */
+ 
+ #include<iostream>
+ using namespace std;
+ 
+ main(){
+ 	int contador = 1;
+ 	while(contador <= 20){
+ 		cout<<"Debo hacer la tarea "<<contador<<endl;
+ 		contador++;
+	 }
+	 cout<<"Fin del programa";
+ }
+
+
